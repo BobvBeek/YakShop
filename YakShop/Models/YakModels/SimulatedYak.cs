bb4591d5acd288efd240fb9a07b1eef1c2130ea0
BobvBeek <55236696+BobvBeek@users.noreply.github.com>
2025-06-25 01:54:42 +0200

    Delete YakShop/Models/YakModels/SimulatedYak.cs

--- YakShop/Models/YakModels/SimulatedYak.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YakShop.Models.YakModels
-{
-    public class SimulatedYak
-    {
-        public string Name { get; set; } = default!;
-        public double AgeInYears { get; set; }
-        public double AgeLastShaved { get; set; }
-    }
-}
